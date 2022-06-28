468d649dc8f55ce266b1800edbd5ffd82ae8d3d1
Sakar Adhikari <be2020se717@gces.edu.np>
2022-06-28 19:43:01 +0545

    ok

---
+++ Assignment/assignment 9/student/action/delete.php
@@ -0,0 +1,17 @@
+<?php 
+// include_once 'connect.php';
+require_once "../../utils/db.php";
+if(isset($_GET['deleteid'])){
+    $id = $_GET['deleteid'];
+
+    $sql = "delete from `students` where id=$id";
+    $result = mysqli_query($conn,$sql);
+    if($result){
+        header('location:../');
+        // echo "sucess";
+    }
+    else{
+        // echo "hey ho";
+        die(mysqli_error($conn));
+    }
+}
